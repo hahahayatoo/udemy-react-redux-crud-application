c2b5afa349b4458385e6c5caf00c8d3f0fec0a58
hahahayatoo <hzzkwepufd@gmail.com>
2019-11-30 14:47:05 +0900

    update: create class components and functional components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+// import { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-    // React の return で返すタグは一つだけという制約があるため、
-    // React.Fragment で label と input を囲っている
-    // この React.Fragment は HTML 上には出力されない
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I'm changed")}} />
-    </React.Fragment>
-    )
-  }
+// // Component を継承している APP は class component
+// class App extends Component {
+//   render() {
+//     return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("I'm changed")}} />
+//     </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
+}
+
+const Cat = () => {
+  return <div>Meow!</div>;
 }
 
 export default App;
