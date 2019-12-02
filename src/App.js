942e71de3f93c9a0b83d42e6a372dd56fb751e73
hahahayatoo <hzzkwepufd@gmail.com>
2019-12-02 20:54:35 +0900

    update: implement action and action creater

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
+// パッケージを追加する必要ため、下記のコマンドを実行
+// yarn add redux react-redux
 import React, { Component } from 'react';
-
-// state はミュータブルな値で props はイミュータブルな値
 
 const App = () => (<Counter></Counter>)
 
 class Counter extends Component  {
-  // Counter 呼び出し時に constructor が呼び出される
   constructor(props) {
     super(props)
     this.state = { count: 0 }
   }
 
   handlePlusButton = () => {
-    // React では setState() のコールバックとして Render() が呼び出されるため、
-    // 値の更新は setState() を利用すること
     this.setState({ count: this.state.count + 1 })
-
-    // ↓のように state を直接変更しようとすると
-    // Do not mutate state directly. Use setState()
-    // のように state を直接変更せずに setState() を利用するよう言われ、
-    // カウンタの値も更新されない
-    // this.state = { count: this.state.count + 1 }
   }
 
   handlMinusButton = () => {
