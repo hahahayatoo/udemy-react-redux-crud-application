4e05478f2c4ec83e4fa51fa8ac1184cc9080b877
hahahayatoo <hzzkwepufd@gmail.com>
2019-12-08 13:16:22 +0900

    update: add comment

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -3,6 +3,7 @@ import { INCREMENT, DECREMENT } from "../actions";
 // 状態の初期値を定義
 const initialState = { value: 0 }
 
+// この reducer が実際の状態を持っている
 export default ( state = initialState, action ) => {
     switch ( action.type ) {
         case INCREMENT:
